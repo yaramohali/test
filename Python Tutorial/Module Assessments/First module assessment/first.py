e1798a5e739eb163886234b052970b72c39d3c5a
yaramohamed <1901589@eng.asu.edu.eg>
2024-12-28 02:09:23 +0200

    first mini-project

---
+++ Python Tutorial/Module Assessments/First module assessment/first.py
@@ -0,0 +1,9 @@
+def main():
+    name = "Yara Mohamed Ali"
+    age = 23
+    summary = "i want to learn how to use python in software testing"
+    
+    print(name,age,summary)
+    
+if __name__ == "__main__":
+    main()
