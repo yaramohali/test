83b41be12eacade4e29a66176811762d08acdc82
yaramohamed <1901589@eng.asu.edu.eg>
2024-12-28 03:16:31 +0200

    Calculator

---
+++ Python Tutorial/Module Assessments/Calculator/main.py
@@ -0,0 +1,13 @@
+def main():
+    
+    first_number = int(input("Please enter first number"))
+    second_number = int(input("Please enter second number"))
+    summation = first_number + second_number
+    subtraction = first_number - second_number 
+    multiplication = first_number * second_number 
+    division = first_number / second_number 
+    print(summation,subtraction,multiplication,division)
+
+
+if __name__ == "__main__":
+    main()
